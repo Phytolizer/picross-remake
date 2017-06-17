5a517779b28fff3db87f0fdcbd5e94eecc78d4ba
onContentStop <mariorules24@gmail.com>
2017-06-17 00:39:44 -0500

    Comments because I feel like it.

--- src/mygl/Background.java
+++ src/mygl/Background.java
@@ -4,14 +4,24 @@ import java.awt.*;
 import java.util.List;
 
 /**
- * Created by mario on 1/12/2017.
+ * Background is a class that should make dynamic colors easier for everyone.<br/>It can
+ * be set to generate random colors and shift between them in an arbitrary manner according to one of
+ * the functions defined in the {@link Motion Motion} class. Or it can be given a list of colors to switch between, never selecting
+ * the same color twice. The rate at which it shifts is defined at initialization.
  */
 public class Background {
 	private Color initialColor, destinationColor, currentColor;
 	private int interval;
 	private RandomColorGenerator randomColorGenerator;
 	private Timer timer;
+	private MotionMode motionMode = MotionMode.CIRCLE_QUADRANT;
 
+	/**
+	 * Creates a new Background which will shift between colors in <code>colors</code> randomly and smoothly.
+	 * @param colors The colors this Background will choose between. Can contain any amount of Colors, but it is recommended to use at least 3 for random behavior.
+	 * @param colorTimer A Timer that will help the Background figure out where it is supposed to be in a given instant.
+	 * @param colorIntervalMS The amount of time it should take, in milliseconds, for the Background to switch from one Color to another.
+	 */
 	public Background(List<Color> colors, Timer colorTimer, int colorIntervalMS) {
 		randomColorGenerator = new RandomColorGenerator(colors);
 		initialColor = randomColorGenerator.getNextColor();
@@ -21,6 +31,12 @@ public class Background {
 		interval = colorIntervalMS;
 	}
 
+	/**
+	 * Creates a new Background which will shift between <i>random</i> colors smoothly.
+	 * @param minBrightness The minimum luminance which the Background will permit to switch to. For all possible colors to be eligible, this should be -1.
+	 * @param colorTimer A Timer that will help the Background figure out where it is supposed to be in a given instant.
+	 * @param colorIntervalMS The amount of time it should take, in milliseconds, for the Background to switch from one Color to another.
+	 */
 	public Background(double minBrightness, Timer colorTimer, int colorIntervalMS) {
 		randomColorGenerator = new RandomColorGenerator(minBrightness);
 		initialColor = randomColorGenerator.getNextColor();
@@ -30,6 +46,12 @@ public class Background {
 		interval = colorIntervalMS;
 	}
 
+	/**
+	 * Returns whether two Colors are identical.
+	 * @param c1 A Color
+	 * @param c2 A Color to compare <code>c1</code> against
+	 * @return whether the color of c1 is equal to that of c2
+	 */
 	private static boolean equalColors(Color c1, Color c2) {
 		return c1.getRed() == c2.getRed()
 				&& c1.getGreen() == c2.getGreen()
@@ -37,22 +59,42 @@ public class Background {
 				&& c1.getAlpha() == c2.getAlpha();
 	}
 
+	/**
+	 *
+	 * @return the color of this Background as of when it was last updated; this will be a mixture of <code>initialColor</code> and <code>destinationColor</code>.
+	 */
 	public Color getCurrentColor() {
 		return currentColor;
 	}
 
+	/**
+	 *
+	 * @return the color that the Background is departing from as it shifts between colors.
+	 */
 	public Color getInitialColor() {
 		return initialColor;
 	}
 
+	/**
+	 *
+	 * @return the color that the Background is approaching as it shifts between colors.
+	 */
 	public Color getDestinationColor() {
 		return destinationColor;
 	}
 
+	/**
+	 *
+	 * @return the amount of time it will take for the Background to go from one color to another
+	 */
 	public int getColorIntervalMS() {
 		return interval;
 	}
 
+	/**
+	 * Uses the {@link Timer timer} provided to calculate the current color of this Background. Will also determine the next Color in sequence if it has
+	 * arrived at {@link Background#destinationColor destinationColor}.
+	 */
 	public void update() {
 		if (timer != null) {
 			if (equalColors(currentColor, destinationColor)) {
@@ -66,14 +108,29 @@ public class Background {
 				progress = 1;
 			}
 			int r, g, b;
-			r = Motion.getNumberBetween(MotionMode.CIRCLE_QUADRANT, initialColor.getRed(), destinationColor.getRed(), progress);
-			g = Motion.getNumberBetween(MotionMode.CIRCLE_QUADRANT, initialColor.getGreen(), destinationColor.getGreen(), progress);
-			b = Motion.getNumberBetween(MotionMode.CIRCLE_QUADRANT, initialColor.getBlue(), destinationColor.getBlue(), progress);
+			r = Motion.getNumberBetween(motionMode, initialColor.getRed(), destinationColor.getRed(), progress);
+			g = Motion.getNumberBetween(motionMode, initialColor.getGreen(), destinationColor.getGreen(), progress);
+			b = Motion.getNumberBetween(motionMode, initialColor.getBlue(), destinationColor.getBlue(), progress);
 			currentColor = new Color(r, g, b);
 		}
 	}
 
+	/**
+	 * May be used to change the Timer that the Background relies on for color calculation.
+	 * @param t the new Timer to be used
+	 */
 	public void setTimer(Timer t) {
 		timer = t;
+	}
+
+	/**
+	 * May be used to change how the Background shifts from one color to another. (Default is {@link MotionMode#CIRCLE_QUADRANT circle quadrant})
+	 * <br/>
+	 * The {@link MotionMode MotionMode} follows a particular graph, with (0, 0) as the starting point and (1, 1) as the end point.
+	 * It translates an x-value (time) into a y-value (percentage of the first color to use in the mixture).
+	 * @param newMode the new MotionMode to use when the Background is next updated
+	 */
+	public void setMotionMode(MotionMode newMode) {
+		motionMode = newMode;
 	}
 }
--- src/picross/GameWindow.java
+++ src/picross/GameWindow.java
@@ -15,37 +15,39 @@ public class GameWindow extends Graphics {
 	private ButtonElement b;
 
 	public GameWindow(KeyListener kl) {
-		super("Picross");
-		frame.setKeyHandler(kl);
-		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+		super("Picross"); //Sets things up. If you want details, look in Graphics, it's too much to explain here.
+		frame.setKeyHandler(kl); //TODO a really good solution for key handling that makes everything look pretty
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH); //Maximizes the frame on screen.
 		Timer bgTimer = new Timer();
 		new Thread(bgTimer).start();
 		bgTimer.start();
-		background = new Background(100, bgTimer, 10000);
+		background = new Background(100, bgTimer, 10000); //This background will choose random colors and shift between them smoothly every 10 seconds.
 		initButtons();
 	}
 
 	private void initButtons() {
 		//b is a perfectly centered ButtonElement. No matter what, B will be at the center of the screen.
-		b = new ButtonElement(width / 2, height / 2, 200, 100, this);
-		b.setText("Start Gayme");
-		b.setColor(Color.GREEN);
+		b = new ButtonElement(width / 2, height / 2, 200, 100, this); //Initializes the button with a position, size and graphics context.
+		b.setText("Start Gayme"); //The text to display on the button goes here. The size of this text will be determined automatically by a process unknown to humankind.
+		b.setColor(Color.GREEN); //This is the color that will be used on the background of the button, behind the text and inside the borders.
 		b.setClickListener(() -> {
-			System.out.println("You clicked the button. You Win!");
+			System.out.println("You clicked the button. You Win!"); //This code will be executed whenever the button registers a click.
+			b.setVisible(false);                                    //A click occurs when the left mouse button is released on top of the visible button element.
 		});
 		b.setOnUpdateAction(() -> {
-			b.setX(width / 2);
-			b.setY(height / 2);
+			b.setX(width / 2); //This code is executed on every frame that the button is visible.
+			b.setY(height / 2); //So is this.
 		});
-		b.setVisible(true);
+		b.setVisible(true); //Now the button will be drawn and updated on screen!
 	}
 
 	@Override
 	public void runActions() {
-		updateSize();
-		background.update();
-		draw();
+		updateSize(); //TODO move this to Graphics, it's such a necessity that I should have done it yesterday.
+		background.update(); //This allows the background color to change continuously. (Discretely, but with small enough steps it looks continuous.)
+		draw(); //I'm not explaining this. Just no. There is no way the function is not clear. It draws stuff.
 		try {
+			//This should cause the framerate to max out at maxFPS, though in reality it probably won't reach that value because my code bad.
 			Thread.sleep((long) (1000d / (double) maxFPS));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -53,16 +55,19 @@ public class GameWindow extends Graphics {
 	}
 
 	protected void drawActions() {
-		//draw background - this should ALWAYS be first in the draw!
+		//draw background - this should ALWAYS be first in the draw! I mean duh, it goes in the back...
+		//if you don't have a background, things will look weird when the window is resized because of how that's done.
 		graphics2D.setColor(background.getCurrentColor());
 		graphics2D.fillRect(0, 0, width, height);
 		setFont(new Font("Arial", Font.BOLD, 50));
 		graphics2D.setColor(Color.black);
 		DrawingTools.drawCenteredText(f, "PICROSS", width / 2, TOP_BAR_HEIGHT + 60, graphics2D);
+		//There are some debug tools here. Use them, or don't. I don't really care.
 		//region debug mouse position
 		/*setFont(new Font("Arial", Font.PLAIN, 20));
 		DrawingTools.drawCenteredText(f, "" + frame.mouseX + ", " + frame.mouseY, width / 2, height / 2, art);*/
 		//endregion
 		//b.draw();
+		//^ Hey look, you don't have to do this anymore!
 	}
 }
